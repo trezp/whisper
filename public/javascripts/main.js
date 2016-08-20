b46a5069527186065666702df404ed02dfd66552
Treasure Porth <treasure.rose@gmail.com>
2016-08-19 23:52:46 -0700

    Read more buttons

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,20 @@
 
 $(document).ready(function(){
+
   $('.destroy').on('submit', function(event){
     return confirm("Are you sure you want to delete this entry?");
   });
+
+  $('.entry-body').each(function(){
+    var desiredPostLength = 300;
+    var contentLength = $(this).text().length;
+    $('.read-more').hide();
+    if (contentLength > desiredPostLength) {
+      $(this).css("color", "blue")
+      $(this).find('a').show();
+      $(this).text().substr(desiredPostLength, contentLength);
+    }
+  })
+
+
 });
